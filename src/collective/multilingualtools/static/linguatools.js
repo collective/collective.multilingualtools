d58ee83ce761a09f54fdd24022e226b089f4d2e5
Wolfgang Thomas <thomas@syslab.com>
2013-09-19 11:05:49 +0200

    don't rely on "jq" being defined

--- src/collective/multilingualtools/static/linguatools.js
+++ src/collective/multilingualtools/static/linguatools.js
@@ -1,19 +1,19 @@
-jq(document).ready(function(){
+jQuery(document).ready(function(){
 
 	//Hide (Collapse) the toggle containers on load
-	jq(".toggle_container").hide();
+	jQuery(".toggle_container").hide();
 
 	//Switch the "Open" and "Close" state per click
-	jq(".trigger").toggle(function(){
-		jq(this).addClass("active");
+	jQuery(".trigger").toggle(function(){
+		jQuery(this).addClass("active");
 		}, function () {
-		jq(this).removeClass("active");
+		jQuery(this).removeClass("active");
 	});
 
 
 	//Slide up and down on click
-	jq(".trigger").click(function(){
-		jq(this).next(".toggle_container").slideToggle("medium");
+	jQuery(".trigger").click(function(){
+		jQuery(this).next(".toggle_container").slideToggle("medium");
         });
 
 });
